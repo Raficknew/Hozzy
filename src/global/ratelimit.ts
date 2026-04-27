6e6bf3267213b88196f05bda21564e969be251eb
Raficknew <rafi20302@gmail.com>
2026-04-27 09:25:15 +0200

    fix: redis fix on ci

--- src/global/ratelimit.ts
+++ src/global/ratelimit.ts
@@ -2,17 +2,24 @@
 import { Ratelimit } from "@upstash/ratelimit";
 import { Redis } from "@upstash/redis";
 
-const redis = Redis.fromEnv();
+let ratelimitForTransactions: Ratelimit | null = null;
 
-const ratelimitForTransactions = new Ratelimit({
-  redis: redis,
-  limiter: Ratelimit.slidingWindow(30, "1m"),
-});
+function getRateLimiter() {
+  if (!ratelimitForTransactions) {
+    const redis = Redis.fromEnv();
+    ratelimitForTransactions = new Ratelimit({
+      redis: redis,
+      limiter: Ratelimit.slidingWindow(30, "1m"),
+    });
+  }
+  return ratelimitForTransactions;
+}
 
 export async function assertTransactionsRateLimit(userId: string) {
   if (!userId) return;
+  if (process.env.CI) return;
 
-  const { remaining } = await ratelimitForTransactions.limit(userId);
+  const { remaining } = await getRateLimiter().limit(userId);
 
   if (remaining <= 0) {
     throw new Error("RateLimitExceededException");
